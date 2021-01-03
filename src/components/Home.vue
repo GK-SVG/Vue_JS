3de1bff312cfe2315867e75116197e7a51194893
gk-svg <gk32239@gmail.com>
2021-01-03 17:24:59 +0530

    components in vue js

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello this is home component</h1>
+        <h1>{{data}}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name:"Home",
+    props:{
+        data:String
+    }
+})
+</script>
