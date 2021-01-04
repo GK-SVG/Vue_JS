212320877c1d6f4e8d9450ef7f6ee83525f152e0
gk-svg <gk32239@gmail.com>
2021-01-04 10:55:29 +0530

    for loop in vue js

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Home data="Tstings props in Vue" />
+    <ForLoop />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue';
+import ForLoop from "./components/loop.vue";
 export default {
   name: 'App',
   components: {
     // HelloWorld,
     Home,
+    ForLoop,
   }
 }
 </script>
---
+++ src/components/loop.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <table border="1px">
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+            </tr>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{user.id}}</td>
+                <td>{{user.name}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name:"ForLoop",
+    data(){
+        return {
+          users:  [
+                 {'id':1,'name':'apple'},
+                 {'id':2,'name':'linux'},
+                 {'id':3,'name':'hp'}
+            ]
+        }
+    }
+})
+</script>
